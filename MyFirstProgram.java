29f248979a26e50b5b0b14dd832a6d35c42b456c
Emac56 <emacvicente8@gmail.com>
2025-08-27 08:09:32 +0800

    Update MyFirstProgram.java

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,1 +1,105 @@
+//* Created bt Jaymark Vicente
+*
+*/
+public class MyFirstProgram {
 
+  public static void main(String arg) {
+    //TITLE
+    System.out.println("Title: My Heart is yours");
+        System.out.println("Performed by: Passion Kristian Stanfill");
+       System.out.println("");
+       System.out.println("");
+      //VERSE1 
+    System.out.println("[Verse 1]");
+       System.out.println("I give you my life");
+       System.out.println("I give you my trust");
+      System.out.println("Jesus");
+      System.out.println("For, you are my God");
+       System.out.println("And you are enough");
+       System.out.println("Jesus,Jesus");
+       System.out.println("");
+       System.out.println("");
+       System.out.println("");
+     System.out.println("I give you my life");
+       System.out.println("I give you my trust");
+      System.out.println("Jesus");
+      System.out.println("And you are my God");
+       System.out.println("And you are enough");
+       System.out.println("Jesus,Jesus");
+     System.out.println("");
+     System.out.println("");
+        System.out.println("[Chorus]");
+     System.out.println("My heart is yours");
+     System.out.println("My heart is yours");
+     System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+    System.out.println("My heart is yours");
+     System.out.println("My heart is yours");
+     System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("[Verse 2]");
+        System.out.println("I lay down my life");
+        System.out.println("I take up my cross");
+        System.out.println("Jesus");
+        System.out.println("For, you are my God");
+        System.out.println("Whatever the cost");
+        System.out.println("Jesus,oh,Jesus");
+        System.out.println("");
+    System.out.println("");
+    System.out.println("My heart is yours");
+     System.out.println("My heart is yours");
+     System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+    System.out.println("My heart is yours");
+     System.out.println(Lord, my heart is yours");
+     System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+    System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+      System.out.println("");
+      System.out.println("");
+      System.out.println("[Bridge]");
+      System.out.println("And all to Jesus I surrender");
+      System.out.println("All to you i freely give");
+      System.out.println("Oh, I will ever love and trust you");
+      System.out.println("In your presence i will live");
+      System.out.println("");
+      System.out.println("");
+      System.out.println("[Chorus]");
+    System.out.println("My heart is yours");
+     System.out.println("My heart is yours");
+     System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+    System.out.println("My heart is yours");
+     System.out.println("My heart is yours");
+     System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+    System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+    ystem.out.println("My heart is yours");
+     System.out.println("My heart is yours");
+     System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+    ystem.out.println("My heart is yours");
+     System.out.println("My heart is yours");
+     System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands Lord");
+    ystem.out.println("My heart is yours");
+     System.out.println("My heart is yours");
+     System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+    System.out.println("Take it all, take it all");
+     System.out.println("My life in your hands");
+    System.out.println("");
+  }
+}
+
+    
+    
+    
+    
+    
+    
+    
