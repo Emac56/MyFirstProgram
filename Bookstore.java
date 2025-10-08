06fd23f1182bdac8c645c521fab720ad9a3c7482
Emac56 <emacvicente8@gmail.com>
2025-10-08 20:01:50 +0800

    Create Bookstore.java

---
+++ Bookstore.java
@@ -0,0 +1,40 @@
+/**Activity 3 ito sa computer programming 1
+ * so bali ang gagawa ka ng isang program na may
+ * pangalan ng item
+ * merong price
+ * merong quantity(kung ilan ang item)
+ * at merong total cost
+ * ang instruction i mumultiply mo ang quantity sa price 
+ * para makuha ang totalcost
+ */
+ import java.util.Scanner;
+public class Bookstore {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(Syste)
+        String itemname = "Notebook";//pangalan ng item
+        float price = 2.5f;//presyo ng item
+        int quantity = 4;//bilang ng item
+        float totalcost = price * quantity;//pinag multiply 
+        //ang value ng  price
+        //which is 2.5 sa value ng quantity which is 4
+        
+        
+        
+        System.out.println("************************************");
+        System.out.println("    Welcome to Java Bookstore");
+        System.out.println("          Omsc Sablayan");
+        System.out.println(" Poblacion, Sablayan, Occ. Mindoro");
+        System.out.println("     Phone number:09101129345");
+        System.out.println("        Omsc.bookstore.com");
+        System.out.println("");
+        System.out.println("      Reg. number # 12345678");
+        System.out.println("");
+        System.out.println(" Sales to customer # Jaymark Vicente");
+        System.out.println("");
+        System.out.println("Item:       " + itemname);
+        System.out.println("Quantity:   " + quantity);
+        System.out.println("Total Cost $" + totalcost);
+        System.out.println("**************************************");
+    }
+},
